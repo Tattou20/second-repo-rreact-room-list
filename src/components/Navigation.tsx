5f19883f2833c09633e3f432bd431ef6c92afc41
Arivola Ursulla <ursullaarivola@gmail.com>
2024-04-17 14:15:59 -0400

    nav change

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,14 +5,20 @@ import { usePathname } from 'next/navigation';
 const Navigation = () => {
   const pathname = usePathname();
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav style={{ 
+      backgroundColor: '#000000', 
+      padding: '20px', 
+      color: '#ffffff', 
+      fontFamily: 'Arial, sans-serif', 
+      fontSize: '30px'
+    }}>
+      <ul style={{ listStyleType: 'none', padding: 0, margin: 0 }}>
+        <li style={{ display: 'inline-block', marginRight: '50px' }}>
           <Link href='/' className={pathname == '/' ? 'active' : ''}>
             Home
           </Link>
         </li>
-        <li>
+        <li style={{ display: 'inline-block', marginRight: '50px' }}>
           <Link href='/room-list' className={pathname == '/room-list' ? 'active' : ''}>
             Room List
           </Link>
